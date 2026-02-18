09e540b799ef3fc1ebb764608728462a70e3ecdf
Madhukar <madhukarmadhawan@gmail.com>
2026-02-18 17:24:43 +0530

    Add Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import {Analytics} from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: "AI Assistant",
@@ -12,7 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0 }}>{children}</body>
+      <body style={{ margin: 0 }}>{children}
+        <Analytics/>
+      </body>
     </html>
   );
 }
